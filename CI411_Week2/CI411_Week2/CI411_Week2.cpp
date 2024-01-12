e1a7ee4bb31f6d73e54bf07b76d6fbae99d454a7
bonknollie <156436929+bonknollie@users.noreply.github.com>
2024-01-12 13:13:09 +0000

    Week 2 of workshop

---
+++ CI411_Week2/CI411_Week2/CI411_Week2.cpp
@@ -0,0 +1,33 @@
+// CI411_Week2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Noah Kirvan-Cranfield 
+// Week 2 
+// CI411 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Arithmetic Operators
+	cout << "Arithmetic operators" << endl;
+	cout << "----------------------" << endl << endl;
+
+
+	cout << "7 + 3 = " << 7 + 3 << endl;
+	cout << "7 + 3 = " << 7 + 3 << endl;
+	cout << "7 + 3 = " << 7 + 3 << endl;
+
+	cout << "7 / 3 = " << 7 / 3 << endl;
+	cout << "7.0 / 3 = " << 7.0 / 3.0 << endl; 
+	cout << "7 % 3 = " << 7 % 3 << endl;
+
+	cout << "7 + 3 * 5 = " << 7 / 3 * 5 << endl;
+	cout << "7 + 3 * 5  = " << (7 + 3) * 5 << endl;
+
+	// End message
+	cout << endl << endl << " Goodbye " << endl;
+	cout << "--------------------------" << endl << endl;
+
+
+	return 0;
+}//-----
